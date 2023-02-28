5d3d6cecc428e4a005476adc5e12198842c124ed
jvchiu131 <85332493+jvchiu131@users.noreply.github.com>
2023-02-28 15:17:38 +0800

    added some UI

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 require_once "controllers/clientsingle.controller.php";
 require_once "models/clientsingle.model.php";
+
 ?>
 
 <link rel = "stylesheet" href = "views/assets/css/styles.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="views/js/clientsingle.js"></script>
 <script src="views/assets/plugins/sweetalert2/sweet_alert.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
 
 
 <div class="clearfix"></div>
@@ -14,7 +21,7 @@ require_once "models/clientsingle.model.php";
   <div class="container-fluid">
    <div class="row pt-2 pb-2">
      <div class="col-sm-12">
-  	    <h4 class="flex justify-center border border-blue text-red-600 bg-blue-500 py-6">CLIENT INFORMATION</h4>
+  	    <h4 class="flex justify-center border border-blue text-white bg-blue-500 py-6">CLIENT INFORMATION</h4>
      </div>
    </div>
 
@@ -22,56 +29,56 @@ require_once "models/clientsingle.model.php";
       <div class="col-lg-12">
         <form role="form" id="client-form" method="POST" autocomplete="nope">
           <div class="card">
-            <div class="card-body">
+            <div class="card-body ml-32">
                  <input type="text" name="trans_type" id="trans_type" value="New" style="visibility:hidden;" required>
-                 <div class="row grid grid-cols-3 gap 4">
-                      <div class="col-sm-10 col-span-2">
-                          <label for="input-1">Client's Name</label>
-                          <input type="text" class="form-control" id="tns-cname" placeholder="Enter Client Name" name="tns-cname" autocomplete="nope" required>
+                 <div class="row grid grid-cols-3 gap 4 my-2 ">
+                      <div class="col-sm-10 place-self-auto ">
+                          
+                          <input type="text" class="form-control rounded-lg border-2 border-blue-500 " id="tns-cname" placeholder="Enter Client Name" name="tns-cname" autocomplete="nope" required>
                       </div>
 
-                      <div class="col-md-1 ... form-group">
+                      <div class="form-group">
                           <label>&nbsp;</label>
-                          <div class="icheck-material-success">
+                          <div class="icheck-material-success place-self-auto  py-2">
                             <input type="checkbox" id="num-isactive" name="num-isactive" value="1" checked/>
                             <label for="num-isactive">Active</label>
                           </div>
                       </div>
 
-                      <div class="col-sm-1 ... form-group">
-                          <label for="input-2">Client ID</label>
-                          <input type="text" class="form-control" id="tns-clientid" name="tns-clientid" value="">
+                      <div class=" ... form-group place-self-auto ">
+                          
+                          <input type="text" class="form-control rounded-lg border-2 border-blue-500 pr-2" id="tns-clientid" name="tns-clientid" placeholder="Enter Client ID" value="">
                       </div>
                   </div>                 
 
-                 <div class="form-group ...">
-                    <label for="input-3">Address</label>
-                    <input type="text" class="form-control" id="tns-address" placeholder="Enter Address" name="tns-address" autocomplete="nope">
-                 </div>                                 
+                 <div class="row grid grid-cols-3 gap 4 my-2">
 
-                 <div class="row">
-                      <div class="col-sm-6 ... form-group">
-                          <label for="input-4">Landline #</label>
-                          <input type="text" class="form-control" id="num-phone" placeholder="Enter Landline #" name="num-phone" autocomplete="nope">
+                 <div class="form-group flex justify-start border-sm  pr-2">
+                   
+                    <input type="text" class="form-control rounded-lg border-2 border-blue-500 pr-2" id="tns-address" placeholder="Enter Address" name="tns-address" autocomplete="nope">
+                 </div>            
+                      <div class="col-sm-6 place-self-auto form-group ">
+                          
+                          <input type="text" class="form-control rounded-lg border-2 border-blue-500 " id="num-phone" placeholder="Enter Landline #" name="num-phone" autocomplete="nope">
                       </div>
-                      <div class="col-sm-6 ... form-group">
-                          <label for="input-5">Mobile #</label>
-                          <input type="text" class="form-control" id="num-mobile" placeholder="Enter Mobile #" name="num-mobile" autocomplete="nope">
+                      <div class="col-sm-6 place-self-auto form-group ">
+                          
+                          <input type="text" class="form-control rounded-lg border-2 border-blue-500 " id="num-mobile" placeholder="Enter Mobile #" name="num-mobile" autocomplete="nope">
                       </div>
                   </div>
 
-                  <div class="row">
-                      <div class="col-sm-4 form-group">
-                          <label for="input-6">E-Mail</label>
-                          <input type="text" class="form-control" id="tns-email" placeholder="Enter E-mail Address" name="tns-email" autocomplete="nope">
+                  <div class="row grid grid-cols-3 gap 4">
+                      <div class="col-sm-4 form-group ">
+                         
+                          <input type="text" class="form-control  rounded-lg border-2 border-blue-500" id="tns-email" placeholder="Enter E-mail Address" name="tns-email" autocomplete="nope">
                       </div>
-                      <div class="col-sm-4 form-group">
-                          <label for="input-7">Website</label>
-                          <input type="text" class="form-control" id="tns-website" placeholder="Enter Website URL" name="tns-website" autocomplete="nope">
+                      <div class="col-sm-4 form-group ">
+                          
+                          <input type="text" class="form-control rounded-lg border-2 border-blue-500" id="tns-website" placeholder="Enter Website URL" name="tns-website" autocomplete="nope">
                       </div>                      
-                      <div class="col-sm-4 form-group">
-                          <label for="input-8">Contact Person</label>
-                          <input type="text" class="form-control" id="tns-cperson" placeholder="Enter Contact Person" name="tns-cperson" autocomplete="nope">
+                      <div class="col-sm-4 form-group ">
+                          
+                          <input type="text" class="form-control rounded-lg border-2 border-blue-500 " id="tns-cperson" placeholder="Enter Contact Person" name="tns-cperson" autocomplete="nope">
                       </div>                     
                   </div> 
             </div>
@@ -80,15 +87,14 @@ require_once "models/clientsingle.model.php";
               <div class="row">
                 <div class="col-lg-3">
                 </div>
-                <div class="col-lg-9">
+                <div class="col-lg-9 flex justify-end my-6 mr-12">
                   <div class="float-sm-right">
-                    <button type="button" class="btn btn-light btn-round px-5" id="btn-new-client"><i class="fa fa-file"></i>&nbsp;&nbsp;New</button>
+                    <button type="button" class="btn btn-light text-center font-sans border border-blue-500 hover:bg-blue-700 hover:text-white transition duration-300 ease-linear font-bold px-5" id="btn-new-client"><i class="fa fa-file"></i>&nbsp;&nbsp;New</button>
 
-                    <button type="submit" class="btn btn-light btn-round px-5"><i class="fa fa-save"></i>&nbsp;&nbsp;Save</button>
+                    <button type="submit" class="btn btn-light text-center font-sans border border-blue-500 hover:bg-blue-700 hover:text-white transition duration-300 ease-linear font-bold px-5"><i class="fa fa-save"></i>&nbsp;&nbsp;Save</button>
 
-                    <button type="button" class="btn btn-light btn-round px-5" id="btn-search" data-toggle="modal" data-target="#modal-search-client"><i class="icon-search4 mr-2"></i> Search</button> 
-
-                    <button type="button" class="btn btn-light btn-round px-5" id="btn-print-clients"><i class="fa fa-print"></i>&nbsp;&nbsp;Print</button>                          
+                    <button type="button" class="btn btn-light text-center font-sans border border-blue-500 hover:bg-blue-700 hover:text-white transition duration-300 ease-linear font-bold px-5" id="btn-search" data-toggle="modal" data-target="#modal-search-client"><i class="icon-search4 mr-2"></i> Search</button> 
+                          
                   </div>
                 </div>
               </div>
@@ -137,8 +143,6 @@ require_once "models/clientsingle.model.php";
       </div>
     </div>
   </div>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="views/js/clientsingle.js"></script>
 </div>
 
 
